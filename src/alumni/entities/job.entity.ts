efd2fa1db90c6c035fbf8a2e9dd24a6351c51c59
Md Faisal Ahmed <71064436+mdfaisalahmed025@users.noreply.github.com>
2023-05-28 23:43:14 +0600

    change

--- src/alumni/entities/job.entity.ts
+++ src/alumni/entities/job.entity.ts
@@ -3,15 +3,15 @@ import { Alumni } from "./alumnus.entity";
 
 
 @Entity()
-export class Job{
+export class Job {
     @PrimaryGeneratedColumn()
-    jobid:string
-    @Column({default:null})
-    Designation:string
-    @Column()
-    CompanyName:string
-    @Column()
-    City:string
+    jobid: string
+    @Column({ default: null })
+    Designation: string
+    @Column({ default: null })
+    CompanyName: string
+    @Column({ default: null })
+    City: string
     @ManyToOne(() => Alumni, (alumni) => alumni.job)
     alumni: Alumni
 
